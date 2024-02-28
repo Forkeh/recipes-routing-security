92ac32d9a6ffa49fa28e984810ae48ce857803b4
Forkeh <forkeh@gmail.com>
2024-02-28 21:22:31 +0100

    small fixes

--- src/recipes/Categories.tsx
+++ src/recipes/Categories.tsx
@@ -1,30 +1,29 @@
 import { useState, useEffect } from "react";
 import { getCategories } from "../services/apiFacade";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Categories = () => {
-  const [categories, setCategories] = useState<Array<string>>();
+    const [categories, setCategories] = useState<Array<string>>();
 
+    useEffect(() => {
+        getCategories().then((res) => setCategories(res));
+    }, []);
 
-  useEffect(() => {
-    getCategories().then((res) => setCategories(res));
-  }, []);
+    return (
+        <>
+            <h2>Categories</h2>
+            <p>Browse recipes by category.</p>
 
-  return (
-    <>
-      <h2>Categories</h2>
-      <p>Browse recipes by category.</p>
-
-      <ul>
-        {categories?.map((category) => (
-          <li key={category}>
-            {category}
-            <Link to={`/recipes?category=${category}`}>{category}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+            <ul>
+                {categories?.map((category) => (
+                    <li key={category}>
+                        {category}
+                        <Link to={`/recipes?category=${category}`}>{category}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 };
 
 export const Desktops = () => <h3>Desktop PC Page</h3>;
--- src/recipes/Contact.tsx
+++ src/recipes/Contact.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-
 export default function Contact() {
-  return (
-    <>
-        <h2>Contact Info here!</h2>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, soluta consectetur. Mollitia omnis aspernatur perferendis cumque eum unde reiciendis ratione nam ipsam voluptate, culpa voluptas odio et sit debitis ut?</div>
-    </>
-  )
+    return (
+        <>
+            <h2>Contact Info here!</h2>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, soluta consectetur. Mollitia omnis
+                aspernatur perferendis cumque eum unde reiciendis ratione nam ipsam voluptate, culpa voluptas odio et
+                sit debitis ut?
+            </div>
+        </>
+    );
 }
