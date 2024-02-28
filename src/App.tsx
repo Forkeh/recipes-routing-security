bae6a40db91a5916a6cd580e77f006de5d7eb56d
Forkeh <forkeh@gmail.com>
2024-02-28 20:42:05 +0100

    finished part 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import Home from "./Home";
 import "./App.css";
 import Contact from "./recipes/Contact";
 import RecipesLayout from "./recipes/RecipesLayout";
+import Logout from "./security/Logout";
+import RequireAuth from "./security/RequireAuth";
 
 export default function App() {
   //const auth = useAuth();
@@ -23,10 +25,18 @@ export default function App() {
                   <Route path=":id" element={<Recipe />} />
                   <Route path="test" element={<h1>Test</h1>} />
               </Route>
-              <Route path="/add" element={<RecipeForm />} />
+              <Route
+                  path="/add"
+                  element={
+                      <RequireAuth roles={["ADMIN"]}>
+                          <RecipeForm />
+                      </RequireAuth>
+                  }
+              />
+
               <Route path="/contact" element={<Contact />} />
               <Route path="/login" element={<Login />} />
-              {/* <Route path="/logout" element={<Logout />} /> */}
+              <Route path="/logout" element={<Logout />} />
               <Route path="*" element={<h1>Page not found ☹</h1>} />
           </Routes>
       </Layout>
--- src/NavHeader.tsx
+++ src/NavHeader.tsx
@@ -1,32 +1,39 @@
 import { Link, NavLink } from "react-router-dom";
 import AuthStatus from "./security/AuthStatus";
+import { useAuth } from "./security/AuthProvider";
 
 export default function NavHeader() {
+  const auth = useAuth();
+
   return (
-    <nav>
-      <ul>
-        <li>
-          {/* <a href="/">Home</a> */}
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          {/* <a href="/categories">Categories</a> */}
-          <NavLink to="/categories">Categories</NavLink>
-        </li>
-        <li>
-          {/* <a href="/recipes">Recipes</a> */}
-          <NavLink to="/recipes">Recipes</NavLink>
-        </li>
-        <li>
-          {/* <a href="/add">Add</a> */}
-          <NavLink to="/add">Add</NavLink>
-        </li>
-        <li>
-          {/* <a href="/add">Add</a> */}
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-        <AuthStatus />
-      </ul>
-    </nav>
+      <nav>
+          <ul>
+              <li>
+                  {/* <a href="/">Home</a> */}
+                  <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                  {/* <a href="/categories">Categories</a> */}
+                  <NavLink to="/categories">Categories</NavLink>
+              </li>
+              <li>
+                  {/* <a href="/recipes">Recipes</a> */}
+                  <NavLink to="/recipes">Recipes</NavLink>
+              </li>
+              <li>
+                  {/* <a href="/add">Add</a> */}
+                  {auth.isLoggedIn() && (
+                      <li>
+                          <NavLink to="/add">Add</NavLink>
+                      </li>
+                  )}
+              </li>
+              <li>
+                  {/* <a href="/add">Add</a> */}
+                  <NavLink to="/contact">Contact</NavLink>
+              </li>
+              <AuthStatus />
+          </ul>
+      </nav>
   );
 }
